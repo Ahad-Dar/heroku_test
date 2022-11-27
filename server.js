4774f1b8fdc4c2578e3e53821b2aa4f000f73eb1
Ahad-Dar <79365354+Ahad-Dar@users.noreply.github.com>
2022-11-27 09:35:57 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var LED = require('zetta-led-mock-driver');
 zetta()
   .name('FirstName-LastName')
   .use(LED)
-  .link('https://web-production-d01e.up.railway.app/')
+  //.link('https://web-production-d01e.up.railway.app/')
   .listen(1337, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });
