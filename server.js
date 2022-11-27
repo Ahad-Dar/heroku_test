7a13b292688a61264a022dfab5c25c833e0d737d
Ahad-Dar <79365354+Ahad-Dar@users.noreply.github.com>
2022-11-27 09:34:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var LED = require('zetta-led-mock-driver');
 zetta()
   .name('FirstName-LastName')
   .use(LED)
-  //.link('https://git.heroku.com/aqueous-hollows-43439.git')
-  .listen(0.0.0.0:1337, function(){
+  .link('https://web-production-d01e.up.railway.app/')
+  .listen(1337, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });
