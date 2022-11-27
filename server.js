40260f159ce0414c9278dfad9e7cd2a6c45de508
Ahad-Dar <79365354+Ahad-Dar@users.noreply.github.com>
2022-11-27 09:39:07 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 process.EventEmitter = require('events').EventEmitter;
 var zetta = require('zetta');
 var LED = require('zetta-led-mock-driver');
+const port=process.env.PORT || 3000
 zetta()
   .name('FirstName-LastName')
   .use(LED)
   //.link('https://web-production-d01e.up.railway.app/')
-  .listen(1337, function(){
-     console.log('Zetta is running at http://127.0.0.1:1337');
+  .listen(3000, function(){
+     console.log('Zetta is running at http://127.0.0.1:3000');
 });
