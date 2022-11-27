818e75422dfb97e66ec327ec28a17835abff0b39
Ahad-Dar01 <ahad.qasim20@gmail.com>
2022-11-27 08:13:36 -0800

    Added bro

---
+++ server.js
@@ -0,0 +1,10 @@
+process.EventEmitter = require('events').EventEmitter;
+var zetta = require('zetta');
+var LED = require('zetta-led-mock-driver');
+zetta()
+  .name('FirstName-LastName')
+  .use(LED)
+  //.link('https://git.heroku.com/aqueous-hollows-43439.git')
+  .listen(1337, function(){
+     console.log('Zetta is running at http://127.0.0.1:1337');
+});
